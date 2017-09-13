ac1b4f78c707b02cb940778410eb55b9274516d9
Adam Gray <adam@pateo.co>
2017-09-13 16:22:02 +0100

    Make the menu button white for no real reason

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -18,7 +18,7 @@ void Menu::update()
 
 void Menu::render(SDL_Renderer *renderer)
 {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderFillRect(renderer, &rect);
 }
 
