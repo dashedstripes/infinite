e9f653b6936c3f21bb6a2bf8bde48e3a97c48271
Adam Gray <adam@pateo.co>
2017-09-13 13:47:18 +0100

    Resize and redistribute entities

--- src/Level.cpp
+++ src/Level.cpp
@@ -4,19 +4,22 @@
 using namespace std;
 
 Level::Level(int difficulty)
-    : player(640 / 2, (480 / 2) + 100, 10, 10),
+    : player(640 / 2, (480 / 2) + 150, 10, 10),
       enemies({})
 {
   this->difficulty = difficulty;
 
   if (difficulty == 1)
   {
-    enemies.push_back(createEnemy());
+    for (int i = 0; i < 10; i++)
+    {
+      enemies.push_back(createEnemy());
+    }
   }
 
   if (difficulty == 2)
   {
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 20; i++)
     {
       enemies.push_back(createEnemy());
     }
@@ -29,7 +32,7 @@ Level::~Level()
 
 Enemy Level::createEnemy()
 {
-  return Enemy((rand() % (600 - 100) + 1), (rand() % (600 - 200) + 1), 20, 20);
+  return Enemy(rand() % (640 - 0 + 1) + 100, rand() % (250 - 10 + 1) + 10, 20, 20);
 }
 
 void Level::update()
